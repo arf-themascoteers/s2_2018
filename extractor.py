8c5d69c26d58f02e520994703d327d6a88356412
Arif <arif@themascoteers.com>
2024-03-02 19:05:56 +1100

    done

--- extractor.py
+++ extractor.py
@@ -6,6 +6,7 @@ import numpy as np
 SCENE_PATH = "scenes"
 CSVS_PATH = "csvs"
 LUCAS_PATH = "LUCAS-SOIL-2018.csv"
+COMPLETE_CSV = "complete.csv"
 BANDS = ["B01",
          "B02",
          "B03",
@@ -13,7 +14,7 @@ BANDS = ["B01",
          "B05",
          "B06",
          "B07",
-         "B08A",
+         "B8A",
          "B08",
          "B09",
          "B11",
@@ -24,8 +25,13 @@ BANDS = ["B01",
          ]
 
 
-def merge_scene(scene):
-    pass
+def merge_scene(this_csv_path):
+    this_df = pd.read_csv(this_csv_path)
+    if not os.path.exists(COMPLETE_CSV):
+        return this_df
+    complete_df = pd.read_csv(COMPLETE_CSV)
+    complete_df = pd.concat([complete_df, this_df], ignore_index=True)
+    return complete_df
 
 
 def find_tiff(tiffs, band):
@@ -37,47 +43,76 @@ def find_tiff(tiffs, band):
 
 def get_coordinates():
     df = pd.read_csv(LUCAS_PATH)
-    lats = list(df["TH_LAT"])
-    longs = list(df["TH_LONG"])
     coordinates = []
-    for i in range(len(lats)):
-        coordinates.append((lats[i], longs[i]))
+    for index, row in df.iterrows():
+        point_id = row["POINTID"]
+        oc = row["OC"]
+        lat = row["TH_LAT"]
+        lon = row["TH_LONG"]
+        coordinates.append((point_id, lat, lon, oc))
     return coordinates
 
 
 def get_band_values(scene, band):
+    print(f"Processing {band} for {scene}")
     the_scene_path = os.path.join(SCENE_PATH, scene)
     tiffs = os.listdir(the_scene_path)
     tiff = find_tiff(tiffs, band)
-    tiff_path = os.path.join(tiff)
+    tiff_path = os.path.join(the_scene_path, tiff)
     coordinates = get_coordinates()
     pixels = []
     with rio.open(tiff_path) as src:
-        for lat, lon in coordinates:
+        for point_id, lat, lon, oc in coordinates:
             row, col = src.index(lon, lat)
             pixel_value = src.read(1, window=((row, row + 1), (col, col + 1)))
             if pixel_value.shape[0] != 0 and pixel_value.shape[1] != 0:
-                pixels.append([lat, lon, pixel_value[0][0]])
+                pixels.append([point_id, lat, lon, oc, pixel_value[0][0]])
     return pixels
 
 
+def merge_bands(all_bands_pixels_df, band, band_pixels):
+    band_df = pd.DataFrame(band_pixels, columns=["lat","lon","POINTID","oc",band])
+    if all_bands_pixels_df is None:
+        return band_df
+    all_bands_pixels_df = pd.merge(all_bands_pixels_df, band_df[["POINTID",band]], on="POINTID")
+    all_bands_pixels_df.sort_values(by="POINTID", inplace=True)
+    return all_bands_pixels_df
+
+
 def process_scene(scene):
-    df = pd.read_csv(LUCAS_PATH)
+    all_bands_pixels_df = None
     for band in BANDS:
-        pixels = get_band_values(scene, band)
-
-
-
+        band_pixels = get_band_values(scene, band)
+        all_bands_pixels_df = merge_bands(all_bands_pixels_df, band, band_pixels)
 
     if not os.path.exists(CSVS_PATH):
         os.mkdir(CSVS_PATH)
+    all_bands_pixels_df = sanitize(all_bands_pixels_df)
     this_csv_path = os.path.join(CSVS_PATH, f"{scene}.csv")
+    all_bands_pixels_df.to_csv(this_csv_path, index=False)
+    merge_scene(this_csv_path)
+
+
+def sanitize(df):
+    df = df[
+            (df["B01"]!=0)
+        |   (df["B02"]!=0)
+        |   (df["B03"]!=0)
+        |   (df["B04"]!=0)
+        |   (df["B05"]!=0)
+        |   (df["B06"]!=0)
+        |   (df["B07"]!=0)
+        |   (df["B8A"]!=0)
+        |   (df["B08"]!=0)
+        |   (df["B09"]!=0)
+        |   (df["B11"]!=0)
+        |   (df["B12"]!=0)
+         ]
+    return df
 
 
 def process_all_scenes():
-    for scene in os.listdir(SCENE_PATH):
-        process_scene(scene)
-        merge_scene(scene)
+    process_all_scenes()
 
 
 if __name__ == "__main__":
